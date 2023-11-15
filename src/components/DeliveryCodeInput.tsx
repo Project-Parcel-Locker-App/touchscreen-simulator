f5a8ffbc265c050e9e1db3b00174b1b057ba1993
Tomoko Takami <t2tato01@students.oamk.fi>
2023-11-15 07:16:57 +0200

    add HTTP request by Axios

--- src/components/DeliveryCodeInput.tsx
+++ src/components/DeliveryCodeInput.tsx
@@ -1,5 +1,5 @@
-
 import React, { useState } from 'react';
+import axios from 'axios';
 import DigitInputBoard from './DigitInputBoard';
 
 interface DeliveryCodeInputProps {
@@ -20,13 +20,41 @@ const DeliveryCodeInput: React.FC<DeliveryCodeInputProps> = ({ onDeliveryCodeSub
   };
 
   const handleClearClick = () => {
-    setDeliveryCode(Array(4).fill(''));
+    const lastFilledIndex = deliveryCode.reduceRight((acc, digit, index) => {
+      if (acc === -1 && digit !== '') {
+        return index;
+      }
+      return acc;
+    }, -1);
+
+    if (lastFilledIndex !== -1) {
+      const updatedCode = [...deliveryCode];
+      updatedCode[lastFilledIndex] = '';
+      setDeliveryCode(updatedCode);
+    }
   };
 
   const handleSubmit = () => {
     // Validate and submit delivery code
     if (deliveryCode.every((digit) => digit !== '')) {
-      onDeliveryCodeSubmit(deliveryCode.join(''));
+      const code = deliveryCode.join('');
+
+      // Send HTTP POST request
+      axios.post('http://localhost:3000/submit-delivery-code', { code })
+        .then((response) => {
+          // Add necessary logic based on the backend response
+          console.log('Delivery code submission successful:', response.data);
+          // Add other necessary logic here if needed
+        })
+        .catch((error) => {
+          console.error('Error submitting delivery code:', error);
+          // Add error handling logic here if needed
+        });
+
+      // Notify the parent component of the delivery code submission
+      onDeliveryCodeSubmit(code);
+
+      // Clear the input
       setDeliveryCode(Array(4).fill(''));
     } else {
       // Handle invalid code
