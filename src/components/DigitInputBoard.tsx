822520629691b91a788d94ae165fd03368525948
Tomoko Takami <t2tato01@students.oamk.fi>
2023-11-23 14:50:10 +0200

    changed InputBoard style

--- src/components/DigitInputBoard.tsx
+++ src/components/DigitInputBoard.tsx
@@ -1,4 +1,55 @@
+import React from 'react';
 
+interface DigitInputBoardProps {
+  onDigitClick: (digit: number) => void;
+  onClearClick: () => void;
+}
+
+const DigitInputBoard: React.FC<DigitInputBoardProps> = ({ onDigitClick, onClearClick }) => {
+  const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        {digits.slice(0, 3).map((digit) => (
+          <button key={digit} onClick={() => onDigitClick(digit)} style={{ width: '50px', height: '50px', margin: '5px' }}>
+            {digit}
+          </button>
+        ))}
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        {digits.slice(3, 6).map((digit) => (
+          <button key={digit} onClick={() => onDigitClick(digit)} style={{ width: '50px', height: '50px', margin: '5px' }}>
+            {digit}
+          </button>
+        ))}
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        {digits.slice(6, 9).map((digit) => (
+          <button key={digit} onClick={() => onDigitClick(digit)} style={{ width: '50px', height: '50px', margin: '5px' }}>
+            {digit}
+          </button>
+        ))}
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        
+        <button onClick={() => onDigitClick(0)} style={{ width: '50px', height: '50px', margin: '5px' }}>
+          0
+        </button>
+        <button onClick={onClearClick} style={{ width: '110px', height: '50px', margin: '5px' }}>
+          Clear
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DigitInputBoard;
+
+
+
+
+/* 
 import React from 'react';
 
 interface DigitInputBoardProps {
@@ -28,3 +79,4 @@ const DigitInputBoard: React.FC<DigitInputBoardProps> = ({ onDigitClick, onClear
 
 export default DigitInputBoard;
 
+ */
