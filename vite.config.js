1364e2b5e271ad6d87654f65bdd8803df0f7f11b
Tomoko Takami <t2tato01@students.oamk.fi>
2023-11-24 15:38:07 +0200

    add outDir: 'dist' to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // ビルドアーティファクトの出力先ディレクトリを 'dist' に変更
+  },
 })
