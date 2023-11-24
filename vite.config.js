5d36b336fce4403501b8eef179248f9457266d37
Tomoko Takami <113787803+t2tato01@users.noreply.github.com>
2023-11-24 15:55:45 +0200

    Update vite.config.js
    change is canceled

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // ビルドアーティファクトの出力先ディレクトリを 'dist' に変更
-  },
 })
